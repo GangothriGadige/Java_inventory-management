5aa588d93e1cbba22daa932b5213d836dd0765ad
Gangothri <gangothrigadige985@gmail.com>
2022-07-25 00:54:49 +0530

    Added an item to the inventory

--- inventory-management/src/main/java/com/tw/inventorymanagement/InventoryController.java
+++ inventory-management/src/main/java/com/tw/inventorymanagement/InventoryController.java
@@ -1,4 +1,22 @@
 package com.tw.inventorymanagement;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestController
 public class InventoryController {
+    private Set<String> inventory = new HashSet<>();
+
+    @PostMapping("/")
+    public ResponseEntity addItem(@RequestBody String itemName) {
+        inventory.add(itemName);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
 }
--- inventory-management/src/test/java/com/tw/inventorymanagement/InventoryControllerTest.java
+++ inventory-management/src/test/java/com/tw/inventorymanagement/InventoryControllerTest.java
@@ -1,4 +1,24 @@
 package com.tw.inventorymanagement;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(InventoryController.class)
 public class InventoryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldAddAnItemToTheInventory() throws Exception {
+        mockMvc.perform(post("/")
+                        .content("Laptop"))
+                .andExpect(status().isCreated());
+
+    }
 }
